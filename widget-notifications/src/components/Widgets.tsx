ca189382f3d619a51006c99a66ad4f8617fe11e8
Arthur Korkiskis Felismino <arthurkfelismino@gmail.com>
2023-07-07 20:17:44 -0300

    Projeto Concluído✅

--- widget-notifications/src/components/Widgets.tsx
+++ widget-notifications/src/components/Widgets.tsx
@@ -11,7 +11,7 @@ export function Widget() {
 
             {/* Seção recente */}
             <section>
-                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-600 dark:text-zinc-300">Recentes</div>
+                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-600 dark:text-zinc-300 font-medium">Recentes</div>
 
                 <div className="divide-y-2 divide-zinc-400 dark:divide-zinc-950">
                     <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
@@ -58,7 +58,7 @@ export function Widget() {
 
             {/* Seção recente */}
             <section>
-                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-300">Antigas</div>
+                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-300 font-medium">Antigas</div>
                 <div className="divide-y-2 divide-zinc-400 dark:divide-zinc-950">
                     <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <BookMarked className="w-6 h-6 text-violet-500 mt-3" />
