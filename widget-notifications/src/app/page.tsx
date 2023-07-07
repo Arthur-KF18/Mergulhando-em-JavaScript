0ee11afa8c538bfb46ac59277f92d89412b390d7
Arthur Korkiskis Felismino <arthurkfelismino@gmail.com>
2023-07-07 20:04:49 -0300

    Mini projeto de notificações Next.js

---
+++ widget-notifications/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Widget } from "../components/Widgets";
+
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-zinc-50 dark:bg-black flex items-center justify-center text-zinc-800 dark:text-zinc-50">
+      <Widget/>
+    </main>
+  )
+}
